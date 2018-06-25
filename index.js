a7f4a6b9601d583a8a632f51ebb74aeb28f16e59
Clive Chu <clivechu@roctec.com.hk>
2018-06-25 12:15:11 +0800

    base

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-  channelId: 1588600822,
-  channelSecret: ba8c4ba770e8254a9af66f4c74957ed3,
-  channelAccessToken: Mn5kESWkl0AYcQebxwVvdEUkOcmpVS5h5KDjhEMz3WFDBO6RyDrhgoKweiI4HMszIh/0AxMeqd93xnwIQUMfao4coGPqSCBlFwNRlkj1MUD0e2rHaQmTSO/2yg9x24tsYyLFeUPjmK882gJdk/d1+gdB04t89/1O/w1cDnyilFU=
+  channelId: "1588600822",
+  channelSecret: "ba8c4ba770e8254a9af66f4c74957ed3",
+  channelAccessToken: "Mn5kESWkl0AYcQebxwVvdEUkOcmpVS5h5KDjhEMz3WFDBO6RyDrhgoKweiI4HMszIh/0AxMeqd93xnwIQUMfao4coGPqSCBlFwNRlkj1MUD0e2rHaQmTSO/2yg9x24tsYyLFeUPjmK882gJdk/d1+gdB04t89/1O/w1cDnyilFU="
 });
 
 bot.on('message', function(event) {
